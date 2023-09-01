312b04261782e8db895892ae53e9becd056be13e
Dosmukhamed Zhanibekov <dozh@Dosmukhameds-MacBook-Pro.local>
2023-09-01 10:50:21 +0800

    cleanup init.lua

--- init.lua
+++ init.lua
@@ -94,7 +94,7 @@ require('lazy').setup({
     {
       -- Adds git related signs to the gutter, as well as utilities for managing changes
       'lewis6991/gitsigns.nvim',
-      enabled = false,
+      event = { 'BufReadPost', 'BufNewFile' },
       opts = {
         -- See `:help gitsigns.txt`
         signs = {
@@ -114,18 +114,6 @@ require('lazy').setup({
         end,
       },
     },
-
-
-
-
-    -- Fuzzy Finder (files, lsp, etc)
-
-
-    -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-    --       These are some example plugins that I've included in the kickstart repository.
-    --       Uncomment any of the lines below to enable them.
-    -- require 'kickstart.plugins.autoformat',
-    -- require 'kickstart.plugins.debug',
 
     { import = "plugins" },
   },
@@ -191,13 +179,17 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
 
 -- [[ Basic Keymaps ]]
-
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.keymap.set('n', '<leader>l', '<cmd>Lazy<cr>', { silent = true })
+vim.keymap.set('n', '<leader>l', '<cmd>Lazy<cr>', { desc = 'Lazy' })
 -- vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
-
+vim.keymap.set("i", "<C-c>", "<Esc>", { desc = 'Exit insert mode' })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Scroll & center down' })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Scroll & center up' })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = 'Copy to clipboard' })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = 'Copy line to clipboard' })
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'Replace with clipboard' })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -214,115 +206,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
-
-
--- [[ Configure Treesitter ]]
--- See `:help nvim-treesitter`
--- require('nvim-treesitter.configs').setup
-
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
--- [[ Configure LSP ]]
---  This function gets run when an LSP connects to a particular buffer.
-
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
--- local servers = {
---   -- clangd = {},
---   -- gopls = {},
---   -- pyright = {},
---   -- rust_analyzer = {},
---   tsserver = {},
---   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
---
---   lua_ls = {
---     Lua = {
---       workspace = { checkThirdParty = false },
---       telemetry = { enable = false },
---     },
---   },
--- }
-
--- Setup neovim lua configuration
-
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
--- Ensure the servers above are installed
--- local mason_lspconfig = require 'mason-lspconfig'
---
--- mason_lspconfig.setup {
---   ensure_installed = vim.tbl_keys(servers),
--- }
---
--- mason_lspconfig.setup_handlers {
---   function(server_name)
---     local on_attach = function(_, bufnr)
---       -- NOTE: Remember that lua is a real programming language, and as such it is possible
---       -- to define small helper and utility functions so you don't have to repeat yourself
---       -- many times.
---       --
---       -- In this case, we create a function that lets us more easily define mappings specific
---       -- for LSP related items. It sets the mode, buffer and description for us each time.
---       local nmap = function(keys, func, desc)
---         if desc then
---           desc = 'LSP: ' .. desc
---         end
---
---         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
---       end
---
---       nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
---       nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
---
---       nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
---       nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
---       nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
---       nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
---       nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
---       nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
---
---       -- See `:help K` for why this keymap
---       nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
---       nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
---
---       -- Lesser used LSP functionality
---       nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
---       nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
---       nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
---       nmap('<leader>wl', function()
---         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
---       end, '[W]orkspace [L]ist Folders')
---
---       -- Create a command `:Format` local to the LSP buffer
---       vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
---         vim.lsp.buf.format()
---       end, { desc = 'Format current buffer with LSP' })
---     end
---
---
---     local capabilities = vim.lsp.protocol.make_client_capabilities()
---     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
---     require('lspconfig')[server_name].setup {
---       capabilities = capabilities,
---       on_attach = on_attach,
---       settings = servers[server_name],
---       filetypes = (servers[server_name] or {}).filetypes,
---     }
---   end,
--- }
-
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
