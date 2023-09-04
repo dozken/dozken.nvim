495acb24d737a5ded40656a77d5758718e2fda34
Dosmukhamed Zhanibekov <dozh@Dosmukhameds-MacBook-Pro.local>
2023-09-05 01:54:42 +0800

    harpoon marks from telescope

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,26 +1,29 @@
 return {
         'ThePrimeagen/harpoon',
+        dependencies = {
+                "nvim-lua/plenary.nvim",
+        },
         keys = function()
                 local map = function(key, func, desc)
                         return {
                                 key,
                                 func,
-                                desc = 'Harpoon ' .. desc
+                                desc = '[H]arpoon ' .. desc
 
                         }
                 end
                 return {
-                        map('<leader>hu', require('harpoon.ui').toggle_quick_menu, 'UI'),
-                        map('<leader>hm', require('harpoon.mark').add_file, 'Mark for Harpoon'),
-                        map('<C-j>', function() require('harpoon.ui').nav_file(1) end, '1'),
-                        map('<C-k>', function() require('harpoon.ui').nav_file(2) end, '2'),
-                        map('<C-l>', function() require('harpoon.ui').nav_file(3) end, '3'),
-                        map('<C-;>', function() require('harpoon.ui').nav_file(4) end, '4'),
-                        map('<leader>hp', require('harpoon.ui').nav_prev, 'prev'),
-                        map('<leader>hn', require('harpoon.ui').nav_next, 'next'),
+                        map('<leader>hm', '<cmd>Telescope harpoon marks<cr>', '[M]arks'),
+                        map('<leader>ha', require('harpoon.mark').add_file, '[A]dd file'),
+                        map('<C-j>', function() require('harpoon.ui').nav_file(1) end, '[1]'),
+                        map('<C-k>', function() require('harpoon.ui').nav_file(2) end, '[2]'),
+                        map('<C-l>', function() require('harpoon.ui').nav_file(3) end, '[3]'),
+                        map('<C-;>', function() require('harpoon.ui').nav_file(4) end, '[4]'),
+                        map('<leader>hp', require('harpoon.ui').nav_prev, '[P]rev'),
+                        map('<leader>hn', require('harpoon.ui').nav_next, '[N]ext'),
                 }
         end,
-        dependencies = {
-                "nvim-lua/plenary.nvim",
-        },
+        config = function()
+                require('telescope').load_extension('harpoon')
+        end
 }
