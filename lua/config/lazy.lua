94132b87ee555b791106ad027912a27b21a3298b
Dosmukhamed Zhanibekov <dozh@Dosmukhameds-MacBook-Pro.local>
2023-09-05 02:57:34 +0800

    lazy configs update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -3,46 +3,42 @@
 --    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-        vim.fn.system {
-                'git',
-                'clone',
-                '--filter=blob:none',
-                'https://github.com/folke/lazy.nvim.git',
-                '--branch=stable', -- latest stable release
-                lazypath,
-        }
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
+  vim.fn.system({ "git", "-C", lazypath, "checkout", "tags/stable" }) -- last stable release
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-        spec = {
-                -- "gc" to comment visual regions/lines
-                {
-                        'echasnovski/mini.comment',
-                        event = { "BufReadPost", "BufNewFile" },
-                        opts = {}
-                },
+  spec = {
+    -- "gc" to comment visual regions/lines
+    {
+      'echasnovski/mini.comment',
+      event = { "BufReadPost", "BufNewFile" },
+      opts = {}
+    },
 
-                { import = "plugins" },
-        },
-        install = { colorscheme = { "tokyonight" } },
-        checker = { enabled = true }, -- automatically check for plugin updates
-        performance = {
-                cache = {
-                        enabled = true,
-                },
-                rtp = {
-                        -- disable some rtp plugins
-                        disabled_plugins = {
-                                "gzip",
-                                -- "matchit",
-                                -- "matchparen",
-                                -- "netrwPlugin",
-                                "tarPlugin",
-                                "tohtml",
-                                "tutor",
-                                "zipPlugin",
-                        },
-                },
-        },
+    { import = "plugins" },
+  },
+  defaults = { lazy = true },
+  install = { colorscheme = { "tokyonight" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
+  performance = {
+    cache = {
+      enabled = true,
+    },
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "rplugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
 })
