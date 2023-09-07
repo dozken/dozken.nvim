3a4d7790ea4b339a180f3a1422881024cc62f3f2
Dosmukhamed Zhanibekov <dozh@Dosmukhameds-MacBook-Pro.local>
2023-09-07 18:07:30 +0800

    comments on remaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,28 +1,47 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+-- NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- [[ Basic Keymaps ]]
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
+
+-- Disable Space key in normal and visual modes (acts as a placeholder)
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Map <leader>l in normal mode to the ':Lazy' command
 vim.keymap.set('n', '<leader>l', '<cmd>Lazy<cr>', { desc = 'Lazy' })
--- vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+
+-- Map <C-c> in insert mode to exit insert mode
 vim.keymap.set("i", "<C-c>", "<Esc>", { desc = 'Exit insert mode' })
+
+-- Map <C-d> in normal mode to scroll down and center the screen
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Scroll & center down' })
+
+-- Map <C-u> in normal mode to scroll up and center the screen
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Scroll & center up' })
+
+-- Copy to clipboard using <leader>y in normal and visual modes
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = 'Copy to clipboard' })
+
+-- Copy the current line to clipboard using <leader>Y in normal mode
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = 'Copy line to clipboard' })
+
+-- Replace selected text with clipboard content using <leader>p in visual mode
 vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'Replace with clipboard' })
 
--- Remap for dealing with word wrap
+-- Execute Ex commands using <leader>pv in normal mode
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Remap k and j in normal mode to handle word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
+-- Create an autocmd to highlight text when yanked (copied)
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
@@ -33,7 +52,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Diagnostic keymaps
+-- Map keys to navigate and manage diagnostics
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+vim.keymap.set('n', '<leader>E', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>Q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
