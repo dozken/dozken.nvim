faf3ea403647fd8897f3941963f1304cde4fdb70
Dosmukhamed Zhanibekov <dozh@Dosmukhameds-MacBook-Pro.local>
2023-11-24 01:05:33 +0800

    rename key desc

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -126,8 +126,8 @@ return {
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',                tag = 'legacy', opts = {} },
-
+      { 'j-hui/fidget.nvim',                opts = {} },
+      --
       -- Additional lua configuration, makes nvim stuff amazing!
       { 'folke/neodev.nvim',                opts = {} },
 
@@ -182,10 +182,10 @@ return {
     },
     config = function(_, opts)
       require('which-key').register({
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+        ['<leader>c'] = { name = 'Code', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = 'Document', _ = 'which_key_ignore' },
+        ['<leader>w'] = { name = 'Workspace', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = 'Rename', _ = 'which_key_ignore' },
         ['<leader>x'] = { name = 'Quickfix', _ = 'which_key_ignore' },
       })
       local servers = opts.servers
