00d3045d650315f255533b2e86af38421560c79e
Dosmukhamed Zhanibekov <dozh@Dosmukhameds-MacBook-Pro.local>
2023-11-29 13:57:47 +0800

    catpuccin!

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,32 @@
 return {
-  -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   enabled = false,
-  --   lazy = false,
-  --   event = { "VimEnter" },
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme 'catppuccin-mocha'
-  --   end,
-  -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    event = { "VimEnter" },
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      intergrations = {
+        treesitter = true,
+        treesitter_context = false,
+        mason = true,
+        cmp = true,
+        harpoon = true,
+        fidget = true,
+        telescope = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+
+      }
+    },
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      vim.cmd.colorscheme 'catppuccin-mocha'
+    end,
+  },
   --
   -- {
   --   'folke/tokyonight.nvim',
@@ -23,29 +40,29 @@ return {
   --   end,
   -- },
 
-  {
-    'rose-pine/neovim',
-    event = { "VimEnter" },
-    enabled = true,
-    name = 'rose-pine',
-    lazy = true,
-    priority = 1000,
-    config = function()
-
-      require('rose-pine').setup({
-        disable_background = true,
-        highlight_groups = {
-          TelescopeBorder = { fg = "highlight_high", bg = "none" },
-          TelescopeNormal = { bg = "none" },
-          TelescopePromptNormal = { bg = "none" },
-          TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-          TelescopeSelection = { fg = "text", bg = "base" },
-          TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
-        },
-      })
-      --
-      -- Set colorscheme after options
-      vim.cmd('colorscheme rose-pine')
-    end,
-  },
+  -- {
+  --   'rose-pine/neovim',
+  --   event = { "VimEnter" },
+  --   enabled = true,
+  --   name = 'rose-pine',
+  --   lazy = true,
+  --   priority = 1000,
+  --   config = function()
+  --
+  --     require('rose-pine').setup({
+  --       disable_background = true,
+  --       highlight_groups = {
+  --         TelescopeBorder = { fg = "highlight_high", bg = "none" },
+  --         TelescopeNormal = { bg = "none" },
+  --         TelescopePromptNormal = { bg = "none" },
+  --         TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+  --         TelescopeSelection = { fg = "text", bg = "base" },
+  --         TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+  --       },
+  --     })
+  --     --
+  --     -- Set colorscheme after options
+  --     vim.cmd('colorscheme rose-pine')
+  --   end,
+  -- },
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,23 +5,26 @@ return {
     vim.opt.shortmess:append({ W = true, I = false, c = true })
   end,
   opts = {
+
     sections = {
       lualine_b = { 'branch', 'diff', 'diagnostics' },
       lualine_c = {
         { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
       },
-      lualine_y = {
-        { "progress", padding = { left = 1, right = 0 }, separator = " " },
-        { "location", padding = { left = 0, right = 1 } },
-      },
-      lualine_z = {
-        function()
-          return " " .. os.date("%R")
-        end,
-      },
+      -- lualine_y = {
+        -- { "progress", padding = { left = 1, right = 0 }, separator = " " },
+        -- { "location", padding = { left = 0, right = 1 } },
+      -- },
+      -- lualine_z = {
+      --   function()
+      --     return " " .. os.date("%R")
+      --   end,
+      -- },
     },
     options = {
-      component_separators = '|',
+      theme = 'catppuccin',
+      -- component_separators = { left = '', right = '' },
+      -- component_separators = '|',
       section_separators = '',
     }
   }
